de44caa7bae0d8ffca6a69c71a0005acb868482a
BenBetts24 <bsb83@miami.edu>
2020-02-07 10:24:28 -0500

    Added comments

--- simulations.py
+++ simulations.py
@@ -8,29 +8,36 @@ def weightedAverage(old, new, total):
 
 def writeToFile(moves, times, nodes, gameNum, name):
     print("Writing to file...")
+
+    # Overwrites existing file, file updated after every game
     outFile = open(name, "w")
 
+    # Output semicolon separated for easy parsing later on
     for move in moves.keys():
         outFile.write(str(move) + ";" + str(moves[move]) + ";" + str(times[move]) + ";" + str(nodes[move]) + "\n")
 
     print("done")
     timer.sleep(3)
 
-averageTimes = {}
-averageNodes = {}
-moveCounts = {}
+# Creating dictionaries to store move data
+averageTimes = {} # Avg time spend on each move
+averageNodes = {} # Avg nodes visited
+moveCounts = {} # How many times that move has been played
 gameNum = 0
 fileName = "SimulationData/SimData-" + str(datetime.now())
 
 while True:
     gameNum += 1
 
+    # The next game to be run from which data will be collected
     print("Starting game " + str(gameNum))
     exec(open("./connectFourComp.py").read())
 
+    # Keeping track of which move is being played
     computerOne = True
     moveNum = 0
 
+    # Grabbing data from the game that was just simulated
     input = open("data.txt", "r")
     while True:
         if computerOne:
@@ -65,4 +72,5 @@ while True:
             print("Finished reading data")
             break
 
+    # Once data has been added to dicts, overwrite old file
     writeToFile(moveCounts, averageTimes, averageNodes, gameNum, fileName)
