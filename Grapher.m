f25641b709a20e6446265e38f52b90275ccdfd4e
BenBetts24 <bsb83@miami.edu>
2020-02-09 20:01:31 -0500

    Added script to graph sim data as well as the graphs created

---
+++ Grapher.m
@@ -0,0 +1,23 @@
+%% Program to graph connect 4 alpha-beta simulation data
+clear
+
+totalPruning = readmatrix('SimulationData/TotalSimData.txt');
+totalNoPruning = readmatrix('SimulationData/NoPruningTotalSimData.txt');
+
+subplot(2, 1, 1)
+plot(totalNoPruning(:,1), totalNoPruning(:,3), '-x');
+hold on
+plot(totalPruning(:,1), totalPruning(:,3), '-x');
+title('Time Taken / Move'); 
+xlabel('Turn Number');
+ylabel('Time Taken');
+axis([1, 19, 0, 4]);
+
+subplot(2, 1, 2)
+plot(totalNoPruning(:,1), totalNoPruning(:,4), '-x');
+hold on
+plot(totalPruning(:,1), totalPruning(:,4), '-x');
+title('Nodes Visited / Move'); 
+xlabel('Turn Number');
+ylabel('Nodes Visited');
+axis([1, 19, 0, 3000]);
